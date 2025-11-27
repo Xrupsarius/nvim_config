4db98eeb8eab278664de975c14c4b5dd25d9d3f3
xrups <xrupsas@xrups.xrupsas>
2025-11-27 12:43:01 -0500

    blankline.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,20 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "lua", "python", "rust", "javascript", "html", "css", "json", "java", "sql", "dockerfile" },
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+    {
+      "nvim-treesitter/nvim-treesitter",
+       dependencies = {
+           "nvim-treesitter/nvim-treesitter-refactor",
+       },
+       build = ":TSUpdate",
+       config = function()
+         require("nvim-treesitter.configs").setup({
+           ensure_installed = { "lua", "python", "rust", "javascript", "html", "css", "json", "java", "sql", "dockerfile" },
+           highlight = { enable = true },
+           refactor = {
+               highlight_current_scope = {enable = true},
+           },
+           indent = { enable = true },
+         })
+       end,
+      },
 }
 
